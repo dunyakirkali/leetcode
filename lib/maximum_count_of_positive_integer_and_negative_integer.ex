6d7416a521628958616990341d48a92b18680073
Dunya Kirkali <dunyakirkali@gmail.com>
2023-01-12 09:23:59 +0100

    Update maximum_count_of_positive_integer_and_negative_integer.ex

--- lib/maximum_count_of_positive_integer_and_negative_integer.ex
+++ lib/maximum_count_of_positive_integer_and_negative_integer.ex
@@ -1,4 +1,12 @@
 defmodule MaximumCountOfPositiveIntegerAndNegativeInteger do
+  @doc """
+      iex> MaximumCountOfPositiveIntegerAndNegativeInteger.solve([-2,-1,-1,1,2,3])
+      3
+      iex> MaximumCountOfPositiveIntegerAndNegativeInteger.solve([-3,-2,-1,0,0,1,2])
+      3
+      iex> MaximumCountOfPositiveIntegerAndNegativeInteger.solve([5,20,66,1314])
+      4
+  """
   @spec solve(nums :: [integer]) :: integer
   def maximum_count(nums) do
     nums
